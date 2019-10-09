5bc139784476b0e52ae0a391490c5dfd6d220706
Pheorix <jrix21@gmail.com>
2019-10-08 21:46:19 -0500

    Final

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/eSportsNews", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/eSportsNews", {
   useNewUrlParser: true
 });
 
